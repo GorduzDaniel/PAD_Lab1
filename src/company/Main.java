4f3878f3215d6ff6a6843dbbe804da909a1653a1
Daniel Gorduz <daniel_gorduz@yahoo.com>
2017-11-16 10:29:24 +0200

    first commit

---
+++ src/company/Main.java
@@ -0,0 +1,10 @@
+package company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SocketServer socketServer = new SocketServer();
+        Thread socketThread = new Thread(socketServer);
+        socketThread.start();
+    }
+}
